0e333eecd894f22635cd02862cce785e33d070bb
Ashuthosh0 <161949126+Ashuthosh0@users.noreply.github.com>
2025-05-14 20:38:54 +0530

    Update llm_config.py

--- llm_config.py
+++ llm_config.py
@@ -2,12 +2,10 @@ from langchain_groq.chat_models import ChatGroq
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Define LLM instance once
 llm = ChatGroq(
     model="llama3-70b-8192",
     temperature=0.75,
-    api_key=os.getenv("GROQ_API_KEY"),  # Load API key from .env
+    api_key=os.getenv("GROQ_API_KEY"),  # load API key from env
 )
